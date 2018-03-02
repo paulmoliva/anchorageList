71063a4d2e54e03a9466105a7be0b0a6e31d712c
paulmoliva <paulmoliva@gmail.com>
2018-03-01 17:32:36 -0900

    add react dev tools

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,11 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from '../reducers/rootReducer';
 
+const middleware = applyMiddleware(thunk, logger);
+
+
 const configureStore = (initialState = {}) => {
-  return createStore(rootReducer, initialState, applyMiddleware(thunk, logger));
+  return createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(middleware)
+  );
 };
 
 export default configureStore;
